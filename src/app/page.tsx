f17b9dc56bc3f47750f4a0e9299880e16f16f32e
David Yan <133011480+david2020y@users.noreply.github.com>
2025-03-26 22:27:10 +0800

    app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "@/components/sections/HeroSection";
 import ConceptSection from "@/components/sections/ConceptSection";
-import CoursesSection from "@/components/sections/CoursesSection";
+// 暂时注释掉找不到的组件导入
+// 暂时注释掉 CoursesSection 导入，因为该组件文件不存在
+// import CoursesSection from "@/components/sections/CoursesSection";
 import SpaceSection from "@/components/sections/SpaceSection";
 import EventsSection from "@/components/sections/EventsSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
@@ -11,7 +13,8 @@ export default function Home() {
     <>
       <HeroSection />
       <ConceptSection />
-      <CoursesSection />
+      {/* 暂时注释掉 CoursesSection 组件，因为找不到该组件 */}
+      {/* <CoursesSection /> */}
       <SpaceSection />
       <EventsSection />
       <TestimonialsSection />
